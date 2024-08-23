03afa08d30f7cee2aa40e70f2e6dbc2433b29a75
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-08-24 02:35:01 +0600

    nav style

--- portfolio/src/components/NavBarComponent.vue
+++ portfolio/src/components/NavBarComponent.vue
@@ -1,14 +1,35 @@
 <template>
-    <nav class="bg-gray-800 p-4">
+    <nav
+        :class="['fixed top-0 left-0 w-full p-8 z-50 transition-colors duration-300', isScrolled ? 'bg-theme1' : 'bg-transparent']">
         <div class="container mx-auto flex justify-between items-center">
             <div class="text-white text-lg font-semibold">
-                <a href="#" class="hover:text-gray-300">Rezaul Karim Shaon</a>
+                <a href="#" class="hover:text-gray-300"></a>
             </div>
-            <div class="space-x-4">
-                <a href="#about" class="text-gray-300 hover:text-white">About</a>
-                <a href="#projects" class="text-gray-300 hover:text-white">Projects</a>
-                <a href="#skills" class="text-gray-300 hover:text-white">Skills</a>
-                <a href="#contact" class="text-gray-300 hover:text-white">Contact</a>
+            <div class="hidden md:flex space-x-4">
+                <a href="#about"
+                    :class="[
+                    'px-4 py-3 font-semibold rounded-lg',
+                    isScrolled ? 'text-gray-300 hover:text-theme3' : 'text-theme3 hover:text-white bg-theme1 hover:bg-theme2']">
+                    About
+                </a>
+                <a href="#projects"
+                    :class="[
+                    'px-4 py-3 font-semibold rounded-lg text-gray-300 hover:text-white',
+                    isScrolled ? 'text-gray-300 hover:text-theme3' : 'text-theme3 hover:text-white bg-theme1 hover:bg-theme2']">
+                    Projects
+                </a>
+                <a href="#skills"
+                    :class="[
+                    'px-4 py-3 font-semibold rounded-lg text-gray-300 hover:text-white',
+                    isScrolled ? 'text-gray-300 hover:text-theme3' : 'text-theme3 hover:text-white bg-theme1 hover:bg-theme2']">
+                    Skills
+                </a>
+                <a href="#contact"
+                    :class="[
+                    'px-4 py-3 font-semibold rounded-lg text-gray-300 hover:text-white',
+                    isScrolled ? 'text-gray-300 hover:text-theme3' : 'text-theme3 hover:text-white bg-theme1 hover:bg-theme2']">
+                    Contact
+                </a>
             </div>
             <div class="md:hidden">
                 <button @click="toggleMenu" class="text-gray-300 focus:outline-none">
@@ -20,7 +41,7 @@
                 </button>
             </div>
         </div>
-        <div v-if="isMenuOpen" class="md:hidden">
+        <div v-if="isMenuOpen" class="md:hidden bg-gray-800">
             <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <a href="#about" class="block text-gray-300 hover:text-white">About</a>
                 <a href="#projects" class="block text-gray-300 hover:text-white">Projects</a>
@@ -36,12 +57,22 @@ export default {
     data() {
         return {
             isMenuOpen: false,
+            isScrolled: false,
         };
     },
     methods: {
         toggleMenu() {
             this.isMenuOpen = !this.isMenuOpen;
         },
+        handleScroll() {
+            this.isScrolled = window.scrollY > ((window.innerHeight / 4) * 3);
+        }
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
     },
 };
 </script>
