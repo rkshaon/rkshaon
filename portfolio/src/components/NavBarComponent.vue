dae4aefbd60aaba94d8578590417e70909cf258f
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-08-24 01:06:42 +0600

    about me component added

--- portfolio/src/components/NavBarComponent.vue
+++ portfolio/src/components/NavBarComponent.vue
@@ -2,7 +2,7 @@
     <nav class="bg-gray-800 p-4">
         <div class="container mx-auto flex justify-between items-center">
             <div class="text-white text-lg font-semibold">
-                <a href="#" class="hover:text-gray-300">My Portfolio</a>
+                <a href="#" class="hover:text-gray-300">Rezaul Karim Shaon</a>
             </div>
             <div class="space-x-4">
                 <a href="#about" class="text-gray-300 hover:text-white">About</a>
