99b7f4437af522efac40c027419267c0661e2cd2
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-08-23 17:29:13 +0600

    tailwind css added

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import { createApp } from "vue";
+// import App from "./App.vue";
+import "./assets/tailwind.css";
+
+// createApp(App).mount("#app");
+
 
 createApp(App).mount('#app')
