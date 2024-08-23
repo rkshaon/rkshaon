402c650c8c1487ecd85c2e0a4bf18b1e3413b989
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-08-24 02:35:01 +0600

    nav style

--- portfolio/src/components/BannerComponent.vue
+++ portfolio/src/components/BannerComponent.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="relative bg-gray-900 text-white h-96 flex items-center justify-center">
-        <img src="@/assets/banner/custom-illustration.webp" alt="Background Image"
+    <div class="relative bg-gray-900 text-white h-screen flex items-center justify-center">
+        <img src="@/assets/banner/codesnipet.webp" alt="Background Image"
             class="absolute inset-0 w-full h-full object-cover opacity-50" />
         <div class="relative z-10 text-center">
-            <h1 class="text-4xl md:text-6xl font-bold">Hi, I'm Rezaul Karim Shaon</h1>
-            <p class="mt-4 text-lg md:text-2xl">Software Engineer | Web Developer | Tech Enthusiast</p>
+            <h1 class="text-4xl md:text-6xl" style="">👋, <br>I'm <i class="font-bold">Rezaul Karim
+                    Shaon</i></h1>
+            <p class="mt-4 text-lg md:text-2xl font-bold text-theme3">Problem Solver | Software Engineer | Tech
+                Enthusiast</p>
+            <p class="mt-4 text-lg md:text-2xl mb-20 font-bold text-theme3">Python | JavaScript | PostgreSQL</p>
             <a href="#projects"
-                class="inline-block mt-8 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg">
-                View My Work
+                class="inline-block mt-8 px-6 py-3 bg-theme2 hover:bg-theme1 hover:text-theme3 text-white font-semibold rounded-lg">
+                VIEW MY WORK
             </a>
         </div>
     </div>
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme1: "#1C4645",
+        theme2: "#047067",
+        theme3: "#FFD146",
+      },
+    },
   },
   plugins: [],
 };
